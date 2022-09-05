0e5f50d64f22855ae274a6ac54886009bd4bd9b5
Jeremy Johnson <jeremyjohnson@Jeremys-Mini.hsd1.ut.comcast.net>
2022-09-05 17:16:00 -0600

    made adding comments possible.

--- Main/controllers/index.js
+++ Main/controllers/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const apiRoutes = require('./api/userRoutes');
 const homeRoutes = require('./homeroutes');
 const dashboardRoutes = require('./api/dashboardRoutes');
+const commentRoutes = require('./api/commentRoutes')
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 router.use('/api/dashboard', dashboardRoutes);
+router.use('/api/comments', commentRoutes);
 
 module.exports = router;
